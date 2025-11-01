61a53bd3e1ca1224cb9750c455d6b266300065c7
Shenura Rasheen <131165226+shenurarasheen@users.noreply.github.com>
2025-11-01 10:18:58 +0530

    implement intial file and folder structure

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,6 @@
+export default function SignIn() {
+    return (
+        <h1 className="text-2xl">Sign In</h1>
+    )
+}
+
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+export default function SignOut() {
+    return (
+        <h1 className="text-2xl">Sign Up</h1>
+    )
+}
