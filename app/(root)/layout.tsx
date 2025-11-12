2c22a84f4dfa8bfd76975e143f9dc7daf3f64681
Shenura Rasheen <rasheen27392@gmail.com>
2025-11-12 10:32:28 +0530

    feat: integrate Sentry for error tracking and performance monitoring

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,9 @@
-
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import { log } from "console";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 
 export default async function RootLayout({
@@ -12,6 +12,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const loggedIn = await getLoggedInUser();
+  if (!loggedIn) redirect('/sign-in');
 
   return (
     <main className="flex h-screen w-full font-inter">
